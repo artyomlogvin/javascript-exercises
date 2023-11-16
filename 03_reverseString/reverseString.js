015510373a0b94663504c35a2d0bbfa9f89767e4
Artyom <43935362+artyomlogvin@users.noreply.github.com>
2023-11-16 23:54:28 +0300

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,22 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let array = [];
 
+    let i = 0;
+    while (i < string.length) {
+        array.push(string[i]);
+        i++;
+    }
+
+    array.reverse();
+
+    let result = "";
+    i = 0
+    while (i < array.length) {
+        result += array[i];
+        i++;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
