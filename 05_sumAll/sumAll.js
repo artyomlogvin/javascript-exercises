36f607c2fe4db9ad85d230344750d680e0ea3e43
Artyom <43935362+artyomlogvin@users.noreply.github.com>
2023-11-17 01:03:44 +0300

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, finish) {
+    if (start > finish) {
+        let temp = start;
+        start = finish;
+        finish = temp;
+    } 
 
+    if (start < 0 || finish < 0) return "ERROR";
+
+    if (typeof start !== 'number' || 
+    typeof finish !== 'number') return "ERROR";
+
+    let sum = 0;
+    for (let i = start; i <= finish; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
