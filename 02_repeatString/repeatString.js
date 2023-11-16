f1266dba41b3dfe4cbde5054d11e118b719fcc82
Artyom <43935362+artyomlogvin@users.noreply.github.com>
2023-11-16 23:44:45 +0300

    Complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    
+    let output = "";
+    let i = 0;
+    while (i < num) {
+        output += string;
+        i++;
+    }
+    return output;
 };
 
 // Do not edit below this line
