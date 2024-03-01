0fca1e3c6a3059dc3ddabfa5e3494d9c29e7f45c
rumi <43935362+artyomlogvin@users.noreply.github.com>
2024-03-01 21:39:46 +0300

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,33 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let puncArr = [',', '.', '!', '?', ':', ';', ' '];
 
+    let arr = str.toLowerCase().split('');
+    for (let i = 0; i < arr.length; i++) {
+        if (puncArr.includes(arr[i])) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+
+    console.log(arr.join(''));
+
+    let revArr = str.toLowerCase().split('');
+    for (let i = 0; i < revArr.length; i++) {
+        if (puncArr.includes(revArr[i])) 
+        {
+            revArr.splice(i, 1);
+            i--;
+        }
+    }
+    revArr.reverse();
+
+    console.log(revArr.join(''));
+
+    if (arr.join('') == revArr.join('')) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
