b4427354e9aeeaacf0a82ef16d2634e3592c1a9e
rumi <43935362+artyomlogvin@users.noreply.github.com>
2024-03-01 22:03:43 +0300

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(posVal) {
+    const pos = +posVal;
 
+    if (pos < 0) {
+        return "OOPS";
+    }
+
+    if (pos === 0) return 0;
+
+    let fibArr = [1, 1];
+
+    if (pos === 1 || pos === 2) {
+        return 1;
+    } else {
+        for (let i = 2; i != pos; i++) {
+            fibArr.push(fibArr[i - 1] + fibArr[i - 2]);
+        }
+    }
+
+    return fibArr[pos - 1];
 };
 
 // Do not edit below this line
