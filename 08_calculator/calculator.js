8d8252cd03ae1dee9a13e26f9a592363fe230635
rumi <43935362+artyomlogvin@users.noreply.github.com>
2024-03-01 20:54:53 +0300

    Complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((mul, current) => mul * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) {
+    return 1;
+  }
+  
+  let factorialResult = a;
+  let n = a - 1;
+	while (n > 1) {
+    factorialResult *= n;
+    n--;
+  }
+  return factorialResult;
 };
 
 // Do not edit below this line
