613346db0a9579d01a2bc20d6b0548ea07cdaa14
rumi <43935362+artyomlogvin@users.noreply.github.com>
2024-03-02 12:54:13 +0300

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let result = array.reduce(function(resInd, person, index) {
+        if (isNaN(person.yearOfDeath)) {
+            let age = new Date().getFullYear() - person.yearOfBirth;
+            if (age > resInd[1]) {
+                return [index, age];
+            }
+        } else if (person.yearOfDeath - person.yearOfBirth > resInd[1]) {
+            return [index, person.yearOfDeath - person.yearOfBirth];
+        } else {
+            return resInd;
+        }
+    }, [0, 0]);
 
+    return array[result[0]];
 };
 
 // Do not edit below this line
